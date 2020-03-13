e6abbdfe4177b0f4a1c738429cee111020a8be51
Luke Gallagher <luke@hypergeometric.net>
2020-03-13 19:59:19 +1100

    Encapsulate FastPFor in implementation file
    This will make link time operations easier, particularly for the test
    suite.

--- include/tesserae/forward_index.hpp
+++ include/tesserae/forward_index.hpp
@@ -7,15 +7,15 @@
 
 #pragma once
 
+#include <algorithm>
+#include <iterator>
+#include <map>
+#include <set>
+#include <vector>
+
 #include "cereal/types/map.hpp"
 #include "cereal/types/utility.hpp"
 #include "cereal/types/vector.hpp"
-
-#include "FastPFor/headers/codecfactory.h"
-#include "FastPFor/headers/deltautil.h"
-
-using namespace FastPForLib;
-IntegerCODEC &codec = *CODECFactory::getFromName("streamvbyte");
 
 class Field {
   uint16_t m_tag_count = 0;
@@ -189,92 +189,8 @@ class Document {
     m_field_stats[field_id].field_len_sum_sqrs(field_len_sum_sqrs);
   }
 
-  void compress() {
-    m_num_terms = m_terms.size();
-    if (0 == m_num_terms) {
-      return;
-    }
-
-    {
-      std::vector<uint32_t> buffer(m_num_terms * 2 + 1024);
-      size_t compressedsize = m_unique_terms.size();
-      Delta::deltaSIMD(m_unique_terms.data(), m_unique_terms.size());
-      codec.encodeArray(m_unique_terms.data(), m_unique_terms.size(),
-                        buffer.data(), compressedsize);
-      buffer.resize(compressedsize);
-      m_unique_terms = buffer;
-    }
-    {
-      std::vector<uint32_t> buffer(m_num_terms);
-      size_t compressedsize = m_terms.size();
-      codec.encodeArray(m_terms.data(), m_terms.size(), buffer.data(),
-                        compressedsize);
-      buffer.resize(compressedsize);
-      m_terms = buffer;
-    }
-    {
-      std::vector<uint32_t> buffer(m_num_terms);
-      size_t compressedsize = m_unique_terms.size();
-      codec.encodeArray(m_freqs.data(), m_freqs.size(), buffer.data(),
-                        compressedsize);
-      buffer.resize(compressedsize);
-      m_freqs = buffer;
-    }
-    {
-      for (auto &&ff : m_field_freqs) {
-        std::vector<uint32_t> buffer(m_num_terms);
-        size_t compressedsize = ff.size();
-        codec.encodeArray(ff.data(), ff.size(), buffer.data(), compressedsize);
-        buffer.resize(compressedsize);
-        ff = buffer;
-      }
-    }
-  }
-
-  void decompress() {
-    if (0 == m_num_terms) {
-      return;
-    }
-
-    {
-      std::vector<uint32_t> terms(m_num_terms);
-      size_t recoveredsize = terms.size();
-      codec.decodeArray(m_unique_terms.data(), m_unique_terms.size(),
-                        terms.data(), recoveredsize);
-      terms.resize(recoveredsize);
-      Delta::inverseDeltaSIMD(terms.data(), terms.size());
-      m_unique_terms = terms;
-    }
-    {
-      std::vector<uint32_t> terms(m_num_terms);
-      size_t recoveredsize = terms.size();
-      codec.decodeArray(m_terms.data(), m_terms.size(), terms.data(),
-                        recoveredsize);
-      terms.resize(recoveredsize);
-      std::vector<uint32_t> tmp;
-      for (auto &&i : terms) {
-        tmp.push_back(m_unique_terms[i]);
-      }
-      m_terms = tmp;
-    }
-    {
-      std::vector<uint32_t> freqs(m_num_terms);
-      size_t recoveredsize = freqs.size();
-      codec.decodeArray(m_freqs.data(), m_freqs.size(), freqs.data(),
-                        recoveredsize);
-      freqs.resize(recoveredsize);
-      m_freqs = freqs;
-    }
-    {
-      for (auto &&ff : m_field_freqs) {
-        std::vector<uint32_t> freqs(m_num_terms);
-        size_t recoveredsize = freqs.size();
-        codec.decodeArray(ff.data(), ff.size(), freqs.data(), recoveredsize);
-        freqs.resize(recoveredsize);
-        ff = freqs;
-      }
-    }
-  }
+  void compress();
+  void decompress();
 
   template <class Archive>
   void serialize(Archive &archive) {
