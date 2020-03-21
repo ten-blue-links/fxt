d915b43b002bb35431f1f704021d93e421f40911
Luke Gallagher <luke@hypergeometric.net>
2020-03-21 15:04:48 +1100

    Fix formatting

--- src/create_doc_lens.cpp
+++ src/create_doc_lens.cpp
@@ -13,45 +13,47 @@
 #include "cereal/archives/binary.hpp"
 
 #include "tesserae/doc_lens.hpp"
+
 int main(int argc, char const *argv[]) {
-    std::string repo_path;
-    std::string doc_lens_file;
+  std::string repo_path;
+  std::string doc_lens_file;
 
-    CLI::App app{"Inverted index generator."};
-    app.add_option("repo_path", repo_path, "Indri repo path")->required();
-    app.add_option("doc_lens_file", doc_lens_file, "Document lens file")->required();
-    CLI11_PARSE(app, argc, argv);
+  CLI::App app{"Inverted index generator."};
+  app.add_option("repo_path", repo_path, "Indri repo path")->required();
+  app.add_option("doc_lens_file", doc_lens_file, "Document lens file")
+      ->required();
+  CLI11_PARSE(app, argc, argv);
 
-    std::ofstream               os(doc_lens_file, std::ios::binary);
-    cereal::BinaryOutputArchive archive(os);
+  std::ofstream os(doc_lens_file, std::ios::binary);
+  cereal::BinaryOutputArchive archive(os);
 
-    indri::collection::Repository repo;
-    repo.openRead(repo_path);
-    indri::collection::Repository::index_state state = repo.indexes();
-    const auto &                               index = (*state)[0];
+  indri::collection::Repository repo;
+  repo.openRead(repo_path);
+  indri::collection::Repository::index_state state = repo.indexes();
+  const auto &index = (*state)[0];
 
-    indri::api::QueryEnvironment indri_env;
-    indri_env.addIndex(repo_path);
+  indri::api::QueryEnvironment indri_env;
+  indri_env.addIndex(repo_path);
 
-    DocLens doc_lens;
-    doc_lens.reserve(index->documentCount() + 1);
-    doc_lens.push_back(0);
+  DocLens doc_lens;
+  doc_lens.reserve(index->documentCount() + 1);
+  doc_lens.push_back(0);
 
-    uint64_t                            docid = index->documentBase();
-    indri::index::TermListFileIterator *iter  = index->termListFileIterator();
-    iter->startIteration();
+  uint64_t docid = index->documentBase();
+  indri::index::TermListFileIterator *iter = index->termListFileIterator();
+  iter->startIteration();
 
-    while (!iter->finished()) {
-        indri::index::TermList *list      = iter->currentEntry();
-        auto &                  doc_terms = list->terms();
-        doc_lens.push_back(doc_terms.size());
-        iter->nextEntry();
-        if (docid % 10000 == 0) {
-            std::cout << "Processed " << docid << " documents." << std::endl;
-        }
-        ++docid;
+  while (!iter->finished()) {
+    indri::index::TermList *list = iter->currentEntry();
+    auto &doc_terms = list->terms();
+    doc_lens.push_back(doc_terms.size());
+    iter->nextEntry();
+    if (docid % 10000 == 0) {
+      std::cout << "Processed " << docid << " documents." << std::endl;
     }
-    delete iter;
-    archive(doc_lens);
-    return 0;
+    ++docid;
+  }
+  delete iter;
+  archive(doc_lens);
+  return 0;
 }
--- src/create_inverted_index.cpp
+++ src/create_inverted_index.cpp
@@ -14,56 +14,60 @@
 #include "tesserae/inverted_index.hpp"
 
 int main(int argc, char const *argv[]) {
-    std::string repo_path;
-    std::string inverted_index_file;
+  std::string repo_path;
+  std::string inverted_index_file;
 
-    CLI::App app{"Inverted index generator."};
-    app.add_option("repo_path", repo_path, "Indri repo path")->required();
-    app.add_option("inverted_index_file", inverted_index_file, "Inverted index file")->required();
-    CLI11_PARSE(app, argc, argv);
+  CLI::App app{"Inverted index generator."};
+  app.add_option("repo_path", repo_path, "Indri repo path")->required();
+  app.add_option("inverted_index_file", inverted_index_file,
+                 "Inverted index file")
+      ->required();
+  CLI11_PARSE(app, argc, argv);
 
-    std::ofstream               os(inverted_index_file, std::ios::binary);
-    cereal::BinaryOutputArchive archive(os);
+  std::ofstream os(inverted_index_file, std::ios::binary);
+  cereal::BinaryOutputArchive archive(os);
 
-    indri::collection::Repository repo;
-    repo.openRead(repo_path);
-    indri::collection::Repository::index_state state = repo.indexes();
-    const auto &                               index = (*state)[0];
+  indri::collection::Repository repo;
+  repo.openRead(repo_path);
+  indri::collection::Repository::index_state state = repo.indexes();
+  const auto &index = (*state)[0];
 
-    {
-        // dump size of vector
-        size_t len = index->uniqueTermCount();
-        archive(len);
+  {
+    // dump size of vector
+    size_t len = index->uniqueTermCount();
+    archive(len);
+  }
+
+  size_t count = 0;
+  indri::index::DocListFileIterator *iter = index->docListFileIterator();
+  iter->startIteration();
+  while (!iter->finished()) {
+    indri::index::DocListFileIterator::DocListData *entry =
+        iter->currentEntry();
+    entry->iterator->startIteration();
+
+    indri::index::TermData *termData = entry->termData;
+
+    PostingList pl(termData->term, termData->corpus.totalCount);
+    std::vector<uint32_t> docs;
+    std::vector<uint32_t> freqs;
+
+    while (!entry->iterator->finished()) {
+      indri::index::DocListIterator::DocumentData *doc =
+          entry->iterator->currentEntry();
+      docs.push_back(doc->document);
+      freqs.push_back(doc->positions.size());
+      entry->iterator->nextEntry();
     }
-
-    size_t                             count = 0;
-    indri::index::DocListFileIterator *iter  = index->docListFileIterator();
-    iter->startIteration();
-    while (!iter->finished()) {
-        indri::index::DocListFileIterator::DocListData *entry = iter->currentEntry();
-        entry->iterator->startIteration();
-
-        indri::index::TermData *termData = entry->termData;
-
-        PostingList           pl(termData->term, termData->corpus.totalCount);
-        std::vector<uint32_t> docs;
-        std::vector<uint32_t> freqs;
-
-        while (!entry->iterator->finished()) {
-            indri::index::DocListIterator::DocumentData *doc = entry->iterator->currentEntry();
-            docs.push_back(doc->document);
-            freqs.push_back(doc->positions.size());
-            entry->iterator->nextEntry();
-        }
-        pl.add_list(docs, freqs);
-        archive(pl);
-        ++count;
-        iter->nextEntry();
-        if (count % 10000 == 0) {
-            std::cout << "Processed " << count << " terms." << std::endl;
-        }
+    pl.add_list(docs, freqs);
+    archive(pl);
+    ++count;
+    iter->nextEntry();
+    if (count % 10000 == 0) {
+      std::cout << "Processed " << count << " terms." << std::endl;
     }
-    std::cout << "Processed " << count << " terms." << std::endl;
-    delete iter;
-    return 0;
+  }
+  std::cout << "Processed " << count << " terms." << std::endl;
+  delete iter;
+  return 0;
 }
